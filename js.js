525eb7a913df72e6fad0967ef60bdc873fb17ba4
jamesd98355 <james.donnelly246@hotmail.com>
2022-07-01 11:35:06 +0100

    works with one digit numbers

--- js.js
+++ js.js
@@ -1,9 +1,50 @@
-let button = document.getElementsByTagName('button');
 const displayValue = document.getElementById('display-value')
 const numberButton = document.getElementsByClassName('number-button')
 const operatorButton = document.getElementsByClassName('operator-button')
 const clearButton = document.getElementById('clear');
-console.log(numberButton)
+const equalsButton = document.getElementById('equals');
+
+
+
+// update sceen on number click
+Array.from(numberButton).forEach((number) => 
+        number.addEventListener('click', () => updateDisplay(number)))
+
+// update screen on uperator click
+Array.from(operatorButton).forEach((number) => 
+    number.addEventListener('click', () => 
+            displayValue.textContent += number.textContent))
+
+// clear screen on clear click
+clearButton.addEventListener('click', clearDisplay)
+
+// update display on equals click
+equalsButton.addEventListener('click', evaluate)
+
+// calculate result from screen value
+//split into array, every second value is operand and every other value is number 
+function evaluate()
+{
+    const screenString = displayValue.textContent;
+    if(displayValue.textContent == "0"){return}
+    const screenArray = screenString.split("");
+    const firstNumber = screenArray[0];
+    const operand = screenArray[1];
+    const secondNumber = screenArray[2];
+
+    const result = operate(operand,firstNumber,secondNumber);
+    displayValue.textContent = result;
+}
+
+function updateDisplay(number){
+    if(displayValue.textContent == 0)
+        {displayValue.textContent = '';}
+    displayValue.textContent += number.textContent;
+}
+
+function clearDisplay(){
+    displayValue.textContent = '0';
+}
 
 
 function operate(operator, a, b){
@@ -16,36 +57,6 @@ function operate(operator, a, b){
         case '÷': return divide(a,b);
     }
 }
-
-
-
-
-
-function updateDisplay(number){
-    if(displayValue.textContent == 0)
-        {displayValue.textContent = ' ';}
-    displayValue.textContent += number.textContent;
-}
-
-function clearDisplay(){
-    displayValue.textContent = '0';
-}
-
-
-
-
-
-
-
-Array.from(numberButton).forEach((number) => 
-        number.addEventListener('click', () => updateDisplay(number)))
-
-Array.from(operatorButton).forEach((number) => 
-number.addEventListener('click', () => 
-displayValue.textContent += number.textContent))
-
-clearButton.addEventListener('click', clearDisplay)
-
 const add = function(a,b) {
 	return a+b;
 };
